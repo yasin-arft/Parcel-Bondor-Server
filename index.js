794ed779f4356eaf42a72e4063768a66ac5c980d
yasin-arft <yasinarftsocial@gmail.com>
2024-06-04 00:45:47 +0600

    inital server setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Parcel Bondor server is running')
+});
+
+app.listen(port, () => {
+  console.log(`Parcel Bondor server running on PORT: ${port}`);
+})
