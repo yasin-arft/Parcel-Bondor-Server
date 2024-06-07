18aa4bcc3b8aa64da52c1c232d12f739c3f74918
yasin-arft <yasinarftsocial@gmail.com>
2024-06-07 13:04:56 +0600

    delete api created for bookings

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -66,6 +66,12 @@ async function run() {
     app.post('/bookings', async (req, res) => {
       const data = req.body;
       const result = await bookingCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.delete('/bookings/:id', async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+      const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
 
